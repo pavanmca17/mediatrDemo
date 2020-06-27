28758bb254df199b41fd48b48c4bd40351cbb9b1
Pavan Kumar Pannala <pavankumar.pannala@outlook.com>
2020-06-27 11:15:45 +0530

    Upgraded to .NET Core 3.0

---
+++ mediatrDemo/Extensions/ServiceExtensions.cs
@@ -0,0 +1,53 @@
+﻿using mediatrDemo.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+
+
+namespace mediatrDemo
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                builder => builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials());
+            });
+        }
+
+        public static void ConfigureValues(this IServiceCollection services, IConfiguration Configuration)
+        {
+            services.Configure<Settings>(options =>
+            {
+                options.MongoDBConnectionString = Configuration.GetSection("DBConnections:MongoConnectionString").Value;
+                options.SqlServerConnectionString = Configuration.GetSection("DBConnections:SqlServerConnectionString").Value;
+                options.NotesDatabase = Configuration.GetSection("DBConnections:NoteDataBase").Value;
+                options.EmployeeDatabase = Configuration.GetSection("DBConnections:EmployeeDatabase").Value;
+                options.Env = Configuration.GetSection("Enviroment:Value").Value;
+               
+            });
+        }
+       
+        public static void AddDBContext<T>(this IServiceCollection services, IConfiguration Configuration) where T : DbContext
+        {
+            services.AddDbContext<T>(options =>
+            {
+                options.UseSqlServer(Configuration.GetSection("DBConnections:SqlServerConnectionString").Value,
+                sqlServerOptionsAction: sqlOptions =>
+                {
+                    sqlOptions.EnableRetryOnFailure(
+                    maxRetryCount: 10,
+                    maxRetryDelay: TimeSpan.FromSeconds(30),
+                    errorNumbersToAdd: null);
+                });
+            });
+        }
+    }
+}
--- mediatrDemo/Startup.cs
+++ mediatrDemo/Startup.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
+﻿using System.Reflection;
+using HealthChecks.UI.Configuration;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace mediatrDemo
 {
@@ -26,9 +21,11 @@ namespace mediatrDemo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Latest);
             services.AddMediatR(Assembly.GetExecutingAssembly());
             services.AddTransient<IMediatorService, MediatorService>();
+            services.AddHealthChecks();
+            services.AddHealthChecks().AddSqlServer(Configuration["DBConnections:SqlServerConnectionString"]);
 
         }
 
@@ -39,8 +36,18 @@ namespace mediatrDemo
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseHealthChecks("/hc");
 
-            app.UseMvc();
+            app.UseHealthChecksUI(delegate (Options options)
+            {
+                options.UIPath = "/hc-ui";
+                
+            });
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
         }
     }
 }
